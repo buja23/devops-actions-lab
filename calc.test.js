32c654c255415262b7a90871f9d92f78ecef80fb
buja23 <jooj.soares.227@gmail.com>
2026-08-27 15:18:39 -0300

    soma

--- calc.test.js
+++ calc.test.js
@@ -1,4 +1,7 @@
+// Importando as funções do calc.js
+const { soma, subtracao, multiplicacao, divisao } = require('./calc');
+
+// O seu teste corrigido (garanta que a matemática está certa!)
 test('soma 2 + 2 deve ser igual a 4', () => {
-  // Alterado para 5 para forçar a falha no pipeline
-  expect(soma(2, 2)).toBe(4); 
+  expect(soma(2, 2)).toBe(4);
 });
